278b2ab362c1e78bc4c8194be5ccdb48dff34472
Pablo Felipe Toledo Coutinho <pablo.coutinho@BRRJOBITAIM1959.local>
2019-05-07 18:48:01 -0300

    Adcionado campo população

--- src/actions/SwApi.js
+++ src/actions/SwApi.js
@@ -1,5 +1,5 @@
 export const swApi = (planetId) => {
-    const apiUrl = "https://swapi.co/api/planets/" + planetId.toString();
+    const apiUrl = "https://swapi.co/api/planets/" + planetId.toString() + "/";
     //const apiUrl = "https://linkquebrado.co/" + planetId.toString();
     
     return fetch(apiUrl, {mode: 'cors'})
